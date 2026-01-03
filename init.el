4b47b06d0ad1a176d9879f49c4dc0a9de8dec08d
Chris Blades <chrisdblades@gmail.com>
2026-01-03 13:17:01 -0500

    upadte, add helm

--- init.el
+++ init.el
@@ -1,11 +1,40 @@
+;configured for version 30.1
+
+					; add melpa repo
+(require 'package)
+(add-to-list 'package-archives
+	     '("MELPA" .
+	       "http://melpa.org/packages/"))
+(package-initialize)
+
 (menu-bar-mode 0)  ; disable menu bar (1 to enable)
 (tool-bar-mode 0)  ; disable tool bar (1 to enable)
 
 
 
-; set auto-save to use temp directory
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-      
+(auto-save-visited-mode)  ; automaticaly safe file
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(dracula-theme gleam-ts-mode helm zig-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+
+(helm-mode 1)
+(global-set-key (kbd "M-x") 'helm-M-x) ; use helm's M-x
+
+
+
+;; theme
+(load-theme 'modus-vivendi-tinted)
