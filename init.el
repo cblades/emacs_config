cd87ec95cc23545e138a0009752f6aa0542c3f54
Chris Blades <chrisdblades@gmail.com>
2026-01-03 13:23:27 -0500

    add company mode

--- init.el
+++ init.el
@@ -14,14 +14,12 @@
 
 (auto-save-visited-mode)  ; automaticaly safe file
 
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(dracula-theme gleam-ts-mode helm zig-mode)))
+ '(package-selected-packages '(company dracula-theme gleam-ts-mode helm zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -31,9 +29,13 @@
 
 
 
+;; helm config 
 (helm-mode 1)
 (global-set-key (kbd "M-x") 'helm-M-x) ; use helm's M-x
 
+
+;; company mode config
+(add-hook 'after-init-hook 'global-company-mode)
 
 
 ;; theme
